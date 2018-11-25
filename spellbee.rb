172060999970a7269cd175d905a6d4e566f764a4
jwillis1010 <45322109+jwillis1010@users.noreply.github.com>
2018-11-25 14:20:46 -0500

    Updated disclaimers.

--- spellbee.rb
+++ spellbee.rb
@@ -2,8 +2,13 @@
 Created by jwillis1010 --> https://github.com/jwillis1010
 in order to help solve the daily NYTimes Spelling Bee Puzzle @ https://www.nytimes.com/puzzles/spelling-bee
 
-Disclaimer: I am not associated with the NYTimes. I'm just a reader who got frustrated trying to find the answers.
-Disclaimer2: Yes, this is cheating. I got bored and decided to write something up to help me solve the riddle.
+Disclaimer 1: I am not associated with the NYTimes. I'm just a reader who got frustrated trying to find the answers.
+
+Disclaimer 2: Yes, this is cheating. I got bored and decided to write something up to help me solve the riddle.
+
+Disclaimer 3: I don't know Ruby all that well and I'm sure this program can be done in 2 lines of code.
+
+Disclaimer 4: The program produced all the answers for the daily puzzle correctly. Tested on 11/25/18.
 
 Thank you to all the contributors of the dwyl/english-words github project
 The dictionary used came from them --> https://github.com/dwyl/english-words
